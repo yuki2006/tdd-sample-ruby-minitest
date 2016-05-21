4de2b5017f537a0418cf404b2d25da83c2f20cf3
kiyohara2 <tomokazu.kiyohara+github2@gmail.com>
2016-05-21 16:54:43 +0900

    Update numerics_spec.rb

--- spec/numerics_spec.rb
+++ spec/numerics_spec.rb
@@ -12,4 +12,16 @@ describe Numerics do
       Numerics.odd?(4) == false
     }
   end
+  
+  it 'even test (true)' do
+    assert {
+      Numerics.even?(4) == true
+    }
+  end
+  
+  it 'even test (false)' do
+    assert {
+      Numerics.even?(3) == false
+    }
+  end
 end
